486404e85ef4ed0eef22f13328969b96475ca1ec
Kazik Pogoda <morisil@xemantic.com>
2020-12-24 02:56:36 +0100

    update to latest fuck-the-ai rules

---
+++ analytics.js
@@ -0,0 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag(){
+  dataLayer.push(arguments);
+}
+
+gtag("js", new Date());
+gtag("config", GA_ID);
+
+loadJs(GA_DIST);
--- custom.js
+++ custom.js
@@ -3,18 +3,6 @@ const cryptedEmail = "bssvpr@krznagvp.pbz";
 
 const GA_DIST = "https://www.googletagmanager.com/gtag/js?" + GA_ID;
 
-window.dataLayer = window.dataLayer || [];
-
-function gtag(){
-  dataLayer.push(arguments);
-}
-
-gtag("js", new Date());
-gtag("config", GA_ID);
-
-// we don't need to wait for this
-addScript(GA_DIST);
-
 const email = cryptedEmail.replace(/[a-zA-Z]/g, c => String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26));
 document.querySelectorAll(".email").forEach(element => {
   if (element.nodeName == "A") {
